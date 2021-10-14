bcd7375bdfb434ed95f783bcedba9793ea843f0c
Nobuyuki Iwanaga <ufcpp@live.jp>
2021-10-14 22:05:02 +0900

    added blank WPF app

---
+++ src/CsharpToHtmlWinApp/App.xaml.cs
@@ -0,0 +1,7 @@
+﻿using System.Windows;
+
+namespace CsharpToHtmlWinApp;
+
+public partial class App : Application
+{
+}
---
+++ src/CsharpToHtmlWinApp/MainWindow.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows;
+
+namespace CsharpToHtmlWinApp;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
